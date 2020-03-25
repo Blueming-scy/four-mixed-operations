806b29231f82a8df215de252f700c562278cb437
Blueming-scy <2782502166@qq.com>
2020-03-25 11:51:14 +0800

    first time push

---
+++ 1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+    int x,y,z,t;    //定义两个操作数x,y，结果z，输入结果t
+    char k;         //运算符k可取“＋、－、×、÷”
+    int i,sum=0;    //题目数量i,答对数目sum
+    srand(time(0));
